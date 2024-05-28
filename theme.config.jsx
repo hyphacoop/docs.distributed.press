290913c7a2279b828c368bd7091f6e662472df70
vincent <vincentcharlebois@gmail.com>
2024-05-28 11:17:35 -0400

    Add analytics script to theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -27,5 +27,6 @@ export default {
   head: (<>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/x-icon" href="/img/favicon-96x96.png"/>
+    <script defer data-domain="docs.distributed.press" src="https://analytics.hypha.coop/js/script.js"></script>
   </>)
 }
