d5b076b1f41a1d2fa4b9612356dd3591872a032b
Akhilesh Thite <akhithite@gmail.com>
2024-11-27 13:56:56 -0800

    docs: add dp-cli

--- components/RouteDescription.jsx
+++ components/RouteDescription.jsx
@@ -1,27 +1,29 @@
-import Pill from "./Pill"
+import Pill from './Pill'
 
-export default function({ httpVerb, path }) {
-  let colour = "#ADADAD"
+export default function ({ httpVerb, path }) {
+  let colour = '#ADADAD'
   switch (httpVerb.toUpperCase()) {
-    case "POST":
-      colour = "#48CA93"
+    case 'POST':
+      colour = '#48CA93'
       break
-    case "GET":
-      colour = "#67AEFA"
+    case 'GET':
+      colour = '#67AEFA'
       break
-    case "DELETE":
-      colour = "#F84D46"
+    case 'DELETE':
+      colour = '#F84D46'
       break
-    case "PUT":
-      colour = "#FBA543"
+    case 'PUT':
+      colour = '#FBA543'
       break
-    case "PATCH":
-      colour = "#50E1C3"
+    case 'PATCH':
+      colour = '#50E1C3'
       break
   }
 
-  return (<div style={{ display: "inline-block", fontSize: "1em", margin: "0.5em 0" }}>
-    <Pill colour={colour}>{httpVerb.toUpperCase()}</Pill>
-    <code style={{ display: "inline-block" }}>{path}</code>
-  </div>)
+  return (
+    <div style={{ display: 'inline-block', fontSize: '1em', margin: '0.5em 0' }}>
+      <Pill colour={colour}>{httpVerb.toUpperCase()}</Pill>
+      <code style={{ display: 'inline-block' }}>{path}</code>
+    </div>
+  )
 }
